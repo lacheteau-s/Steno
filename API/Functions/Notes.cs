a80f8ea1bbc07b712ce5515b945190ab7284a7d7
Sullivan Lacheteau <lacheteau.s@gmail.com>
2025-03-11 17:07:06 -0400

    Configured OpenApi

--- API/Functions/Notes.cs
+++ API/Functions/Notes.cs
@@ -1,7 +1,10 @@
 using API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace API.Functions;
 
@@ -10,6 +13,9 @@ public class Notes(ILogger<Notes> logger)
     private readonly ILogger<Notes> _logger = logger;
 
     [Function("GetNotes")]
+    [OpenApiOperation("GetNotes")]
+    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+    [OpenApiResponseWithBody(System.Net.HttpStatusCode.OK, "application/json", typeof(IEnumerable<NoteModel>))]
     public IResult GetNotes(
         [HttpTrigger(AuthorizationLevel.Function, "get", Route = "notes")] HttpRequest req
     )
