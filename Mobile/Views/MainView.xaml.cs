0372b45af2548c561c22645cb03949b6bc778cb3
Sullivan Lacheteau <lacheteau.s@gmail.com>
2025-03-13 22:28:06 -0400

    Views folder

---
+++ Mobile/Views/MainView.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace Mobile.Views;
+
+public partial class MainView : ContentPage
+{
+    int count = 0;
+
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
