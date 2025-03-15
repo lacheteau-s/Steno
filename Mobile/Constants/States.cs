084074e69cca26c878a3cb743e44b8e0b8b5aeae
Sullivan Lacheteau <lacheteau.s@gmail.com>
2025-03-15 02:17:51 -0400

    State management

---
+++ Mobile/Constants/States.cs
@@ -0,0 +1,7 @@
+﻿namespace Mobile.Constants;
+
+public static class States
+{
+    public const string DEFAULT = nameof(DEFAULT);
+    public const string BUSY = nameof(BUSY);
+}
--- Mobile/ViewModels/CreateNoteViewModel.cs
+++ Mobile/ViewModels/CreateNoteViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Mobile.Constants;
 using Mobile.Services;
 
 namespace Mobile.ViewModels;
@@ -10,6 +11,13 @@ public partial class CreateNoteViewModel : ObservableObject
     private readonly IErrorHandler _errorHandler;
 
     [ObservableProperty]
+    private string _state = States.DEFAULT;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(SaveNoteCommand))]
+    private bool _canStateChange;
+
+    [ObservableProperty]
     private string _content = string.Empty;
 
     public CreateNoteViewModel(IApiClient apiClient, IErrorHandler errorHandler)
@@ -18,16 +26,21 @@ public partial class CreateNoteViewModel : ObservableObject
         _errorHandler = errorHandler;
     }
 
-    [RelayCommand]
+    [RelayCommand(CanExecute = nameof(CanStateChange))]
     private async Task SaveNote()
     {
         try
         {
+            State = States.BUSY;
             await _apiClient.CreateNote(Content);
         }
         catch (Exception ex)
         {
             _errorHandler.HandleError(ex);
+        }
+        finally
+        {
+            State = States.DEFAULT;
         }
     }
 }
