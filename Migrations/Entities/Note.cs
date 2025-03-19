41ca8515ccf41419aa2a7d3667f42414bb5e5725
Sullivan Lacheteau <lacheteau.s@gmail.com>
2025-03-18 23:37:20 -0400

    Parse blob content

---
+++ Migrations/Entities/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace Migrations.Entities;
+
+internal class Note
+{
+    public required string Content { get; set; }
+
+    public required DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public string? ImportChecksum { get; set; }
+}
--- Migrations/Services/ImportManager.cs
+++ Migrations/Services/ImportManager.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using Migrations.Entities;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Migrations.Services;
 
@@ -9,11 +13,43 @@ public partial class ImportManager(ILogger<ImportManager> logger, BlobReader blo
 
     public async Task RunAsync(CancellationToken ct = default)
     {
+        var notes = await ProcessFiles(ct);
+    }
+
+    private async Task<IEnumerable<Note>> ProcessFiles(CancellationToken ct)
+    {
+        var notes = new List<Note>();
         await foreach (var file in _reader.GetFiles(ct))
         {
             _logger.LogInformation("Processing file '{fileName}'", file);
 
             var content = await _reader.ReadFile(file, ct);
+            notes.AddRange(Process(content));
         }
+
+        return notes;
     }
+
+    private IEnumerable<Note> Process(string content)
+    {
+        var matches = EntryRegex().Matches(content);
+        var entities = matches.Select((m, i) =>
+        {
+            var hash = SHA256.HashData(Encoding.UTF8.GetBytes(m.Groups[0].Value));
+            var createdAt = DateTime.Parse(m.Groups["CreatedAt"].Value);
+            var content = m.Groups["Content"].Value.Trim();
+
+            return new Note
+            {
+                ImportChecksum = string.Join("", hash.Select(x => x.ToString("X2"))),
+                CreatedAt = TimeZoneInfo.ConvertTimeToUtc(createdAt, TimeZoneInfo.Local),
+                Content = content
+            };
+        });
+
+        return entities;
+    }
+
+    [GeneratedRegex(@"(?<CreatedAt>\d{4}\/\d{2}\/\d{2} \d{1,2}:\d{2} (?:AM|PM)): (?<Content>.+)", RegexOptions.Compiled)]
+    private static partial Regex EntryRegex();
 }
